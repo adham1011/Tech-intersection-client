9119d428e70259aff25f833d16d8eabcc3a90fc9
mahmod hasan <battleforge-war@hotmail.com>
2019-02-27 22:34:45 +0200

    fixes and almost finished

--- src/reducers/languagesReducer.js
+++ src/reducers/languagesReducer.js
@@ -1,52 +1,85 @@
-import { GET_LANGUAGE_BY_TAGS, GET_LANGUAGES, GET_LANGUAGE, LANGUAGE_LOADING ,GET_LANGUAGE_FOR_COMP, REMOVE_FROM_COMP} from '../actions/types';
+import {
+  GET_LANGUAGE_BY_TAGS,
+  GET_LANGUAGES,
+  GET_LANGUAGE,
+  LANGUAGE_LOADING,
+  GET_LANGUAGE_FOR_COMP,
+  COMP_LANGUAGE_LOADING,
+  REMOVE_FROM_COMP,
+  COMP_RESULTS_LOADING,
+  GET_COMP_RESULTS
+} from "../actions/types";
 
 const initialState = {
-	languages: [],
-	language: {},
-	compLanguages:[],
-	sum: {},
-	loading: false
+  languages: [],
+  language: {},
+  compLanguages: [],
+  sum: {},
+  loading: false,
+  compLoading: false,
+  compResults: {},
+  resultsLoading: false
 };
 
 export default function(state = initialState, action) {
-	switch (action.type) {
-		case GET_LANGUAGES:
-			return {
-				...state,
-				languages: action.payload.languages,
-				sum: action.payload.sum,
-				loading: false
-			};
-		case GET_LANGUAGE:
-			return {
-				...state,
-				language: action.payload,
-				loading: false
-			};
-		case LANGUAGE_LOADING:
-			return {
-				...state,
-				loading: true
-			};
-		case GET_LANGUAGE_BY_TAGS:
-			return {
-				...state,
-				language: action.payload,
-				loading: false
-			};
-		case GET_LANGUAGE_FOR_COMP:
-			return {
-				...state,
-				//compLanguages:action.payload,
-				compLanguages: [...state.compLanguages,action.payload],
-				loading: false
-			};
-		case REMOVE_FROM_COMP:
-		return {
-			...state,
-			compLanguages: state.compLanguages.filter(lang => lang.source !== action.payload),
-		};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case GET_LANGUAGES:
+      return {
+        ...state,
+        languages: action.payload.languages,
+        sum: action.payload.sum,
+        loading: false
+      };
+    case GET_LANGUAGE:
+      return {
+        ...state,
+        language: action.payload,
+        loading: false
+	  };
+	  //results
+    case GET_COMP_RESULTS:
+      return {
+        ...state,
+        compResults: action.payload,
+        resultsLoading: false
+      };
+    case COMP_RESULTS_LOADING:
+      return {
+        ...state,
+        resultsLoading: true
+	  };
+	  
+    case LANGUAGE_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case COMP_LANGUAGE_LOADING:
+      return {
+        ...state,
+        compLoading: true
+      };
+    case GET_LANGUAGE_BY_TAGS:
+      return {
+        ...state,
+        language: action.payload,
+        compLoading: false
+      };
+    case GET_LANGUAGE_FOR_COMP:
+      return {
+        ...state,
+        //compLanguages:action.payload,
+        compLanguages: [...state.compLanguages, action.payload],
+        compLoading: false
+      };
+    case REMOVE_FROM_COMP:
+      return {
+        ...state,
+        compLanguages: state.compLanguages.filter(
+          lang => lang.source !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
 }
